dfcda3136a0717f278c560cf79fb4f21df1af024
kadaligeetha <70795651+kadaligeetha@users.noreply.github.com>
2020-09-05 07:00:19 -0600

    Add files via upload

---
+++ history.py
@@ -0,0 +1,101 @@
+s{7}
+s[7]
+s[-1]
+s[-2]
+s[-7]
+s[5]
+s[5]='t'
+s[1: 5]
+s [:]
+s[:10]
+s[0:]
+s[-6 : -1]
+s[-1:-6]
+s[0:-5]
+s[::-1] #step index
+q='mothers's
+print(q)
+q='mothers\'s'
+a='c:\programfiles/newfloder/text.txt'
+print(a)
+a='c:// programfiless/newfloder//text.txt'
+a=r'c:// programfiless/newfloder//text.txt'
+s[[ :]]
+s{: :-1}
+s[ :: -1]
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+class"list"
+class'list'
+L=[2]
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+L=[-2]
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+L
+PRINT(L)
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+L =[4]
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+L[4]
+L[4]=100
+len(1)
+L =[ 1,  9, 16 ,4335 , 'ABC', 12]
+len(1)
+len(L)
+len(l)
+L[2:8]
+L[-8 :-2]
+l[1, 9, 16, 43435, 'abc', 100,, 12.98, 12, 12, 10,  10, 1, 'xyz']
+l[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L.append(55)
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L.append(55)
+L
+L.extend(,1, 2, 3, 4)
+L
+L.ppend([1, 2, 3, 4])
+L
+L.instert(1, 20)
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L.instert(1, 20)
+len(L)
+L[-1]
+l[16]
+L[16]
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L[16]
+L[16][3]
+L.append([1, 2, 3, 4])
+L.extend(,1, 2, 3, 4)
+L..POP()
+L.pop()
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+#adding new wlwment to list we have predefined funnction append
+L.pop(4)
+L=[1, 9, 16, 43435, 'abc', 100, 12.98, 12, 12, 10,  10, 1, 'xyz']
+L.pop(4)
+L
+L[ :: -1]
+L
+print(L)
+L=x=[1, 8, 6, 4, 5]
+l=x.append(12)
+print(L)
+c=a+b
+a =100
+a=200
+c=a+b
+print(c)
+b=200
+c=a+d
+print(b)
+
+## ---(Thu Aug 20 16:38:46 2020)---
+(1, 7, 15, 19, 16, 9, 15, 13)
+class 'tuple'
+(1, 7, 15, 19, 16, 9, 15, 13)
+
+## ---(Fri Aug 28 17:05:24 2020)---
+welcome_note = "hello all  "
+print(welcome_note + "all well")
+s[::-1] #step index
